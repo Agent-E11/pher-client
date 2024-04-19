cf0a48457e25bff2a0ea02e127bf6f7ef62d6c0c
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-19 15:00:41 -0700

    Create cmd to establish a connection to Gopher server

---
+++ cmd/pher/cmd.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	var showHelpShort bool
+	flag.BoolVar(&showHelpShort, "h", false, "show help")
+	var showHelpLong bool
+	flag.BoolVar(&showHelpLong, "help", false, "show help")
+	var port int
+	flag.IntVar(&port, "p", 70, "port number")
+
+	flag.Parse()
+
+	// TODO: Create a `usage` string
+
+	if showHelpShort || showHelpLong {
+		fmt.Println("Help menu has not been implemented yet")
+		os.Exit(0)
+	}
+
+	address := flag.Arg(0)
+
+	if address == "" {
+		fmt.Println("please supply an address to connect to")
+		os.Exit(1)
+	}
+
+	fmt.Println("Address:", address)
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", address, port))
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	// Write an empty line, indicating to the server to "list what you have"
+	// RFC 1436 Page 3, Paragraph 1 (?)
+	conn.Write([]byte("\r\n"))
+
+	for {
+		// Set a timeout for 5 seconds from now
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			break
+		}
+
+		fmt.Printf("Received: %s", buf[:n])
+	}
+
+	fmt.Println("Done")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/agent-e11/pher-client
+
+go 1.22.1
